9d7b0731a3b91621f6254004a9c889abfdff17c5
Ariana Tran <ariana192000@hotmail.com>
2021-08-18 22:46:04 -0400

    updated the getMode()

--- HangmanMain.java
+++ HangmanMain.java
@@ -22,7 +22,19 @@ public class HangmanMain {
 
     }
 
+    /**
+     * This method obtains whether the user wants to play against the computer
+     * or another player in the same room.
+     * @param s a Scanner
+     * @return a String that represents the game mode or whether they want to quit
+     */
     public static String getMode(Scanner s) {
+        System.out.println("1. One Player Game\n" +
+                "2. Two Player Game\n" +
+                "q. Quit");
+        String mode = s.nextLine();
 
+        return mode;
     }
+
 }
