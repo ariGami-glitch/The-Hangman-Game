1386845bdba0a044b5f45a4601e3d7af8414ad74
Ariana Tran <ariana192000@hotmail.com>
2021-08-18 22:36:00 -0400

    added a mode interface and methods to obtain the user's input

--- HangmanMain.java
+++ HangmanMain.java
@@ -1,5 +1,28 @@
+import java.util.*;
+
+/**
+ * This class represents where the main program will be running
+ */
 public class HangmanMain {
     public static void main(String[] args) {
-        System.out.println("Hi y'all!");
+        System.out.println("Welcome!");
+        Scanner s = new Scanner(System.in);
+
+    }
+
+    /**
+     * This method obtains the userInput
+     * @param s a Scanner
+     * @return a String which is the user's word
+     */
+    public static String getInput(Scanner s) {
+        System.out.print("Player 1, please input your word: ");
+        String word = s.nextLine();
+        return word;
+
+    }
+
+    public static String getMode(Scanner s) {
+
     }
 }
---
+++ Login.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/**
+ * This class represents the login system for the game of hangman
+ */
+public class Login {
+    private JLabel userLabel;
+    private JTextField userText;
+}
